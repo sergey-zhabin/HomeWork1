c20230dced0434ba93d28489d2ed54a7e735b94e
Sergey <santexnik.86@mail.ru>
2022-06-26 22:12:04 +0300

    Решение 4 задачи

---
+++ L4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine(" Введите число ограничив диапазон проверки ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while ( count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write (count + " ");
+    }
+    count++;
+}
+
+/*Числа которые требуется задать в диапазон поиска:
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
