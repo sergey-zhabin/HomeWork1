86f2ccde1c3989df36155c901c2577ead2fbaefc
Sergey <santexnik.86@mail.ru>
2022-06-26 22:10:48 +0300

    Решение 3 задачи

---
+++ L3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число требующеся проверить ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное ");
+}
+else
+{
+    Console.WriteLine("Число нечетное ");
+}
+/* Числа, которые требуется проверить:
+4 -> да
+-3 -> нет
+7 -> нет*/
